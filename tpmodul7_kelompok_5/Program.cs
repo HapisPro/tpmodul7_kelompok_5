8bfeeb87949ac5b9f98e9b38f64cd193aa71a2c1
bintang1855 <bintang.a.p1855@gmail.com>
2025-04-06 14:00:50 +0700

    Menambahkan class KuliahMahasiswa_103022300058

--- tpmodul7_kelompok_5/Program.cs
+++ tpmodul7_kelompok_5/Program.cs
@@ -15,7 +15,11 @@ namespace tpmodul7_kelompok_5
             Console.WriteLine("");
 
             DataMahasiswa_103022300058 bintang = new DataMahasiswa_103022300058();
+            KuliahMahasiswa_103022300069 kuliahBintang = new KuliahMahasiswa_103022300069();
             bintang.ReadJSON();
+            Console.WriteLine("");
+            kuliahBintang.ReadJSON();
+            Console.WriteLine("");
 
         }
     }
---
+++ tpmodul7_kelompok_5/models/KuliahMahasiswa_103022300058.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_5.models
+{
+    public class KuliahMahasiswa_103022300058
+    {
+        public List<Courses> courses { get; set; }
+
+        public void ReadJSON()
+        {
+            string filePath = Path.GetFullPath("../../../../datas/tp7_2_103022300058.json");
+            string jsonString = File.ReadAllText(filePath);
+
+            KuliahMahasiswa_103022300058 courseList = JsonSerializer.Deserialize<KuliahMahasiswa_103022300058>(jsonString);
+
+            int i = 1;
+            foreach (var course in courseList.courses)
+            {
+                Console.WriteLine($"MK{i} {course.code} - {course.name}");
+                i++;
+            }
+        }
+    }
+}
