aaaed83e1b036eb68b5cdb0b2181221a4b8a8185
alkausarhapis <alkausarhapis@gmail.com>
2025-04-06 11:46:54 +0700

    initial commit

---
+++ tpmodul7_kelompok_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul7_kelompok_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
